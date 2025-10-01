01e225e501c681a097e989cdd9fb4e550b86c6d5
mpearc11 <mpearc11@jhu.edu>
2025-10-01 15:32:03 -0400

    Update module6-streamlit.py

--- module6-streamlit.py
+++ module6-streamlit.py
@@ -116,7 +116,8 @@ if st.button('read in clustal alignment file'):
             residue_df = ''
             temp_list = []
             #make a list w numbers for 1 through number of residues
-            num_resi = [1:af3ps_df.iloc[-1,8]
+            st.write(af3ps_df.iloc[-1,8])
+            num_resi = list(range(1,af3ps_df.iloc[-1,8])
             st.write(len(num_resi))
             '''
             for i in 
