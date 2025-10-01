17194d7331531881e5805fd5e1a9ec55a0786344
mpearc11 <mpearc11@jhu.edu>
2025-10-01 11:48:42 -0400

    Update module6-streamlit.py

--- module6-streamlit.py
+++ module6-streamlit.py
@@ -97,18 +97,18 @@ if st.button('read in clustal alignment file'):
             temp_split = temp.splitlines()
             for line in temp_split:
                 if line.startswith('ATOM'):
-                    pLDDT_line = line
+                    pLDDT_line = re.sub(r'\s+', ' ', line)
                     pLDDT_ps += [pLDDT_line]
             #st.write(pLDDT_ps)
-            cleaned_pLDDTps = re.sub(r'\s+', ' ', pLDDT_ps)
-            for idx, item in enumerate(cleaned_pLDDTps):
+            #cleaned_pLDDTps = re.sub(r'\s+', ' ', pLDDT_ps)
+            for idx, item in enumerate(pLDDT_ps):
                 split = item.split(' ')
                 #for item in split:
                     #st.write(idx)
                     #st.write(item)
                 st.write(len(split))
             #turn list from parsed cif file into dataframe
-            af3ps_df = pd.DataFrame(cleaned_pLDDTps, columns=['atom'])
+            af3ps_df = pd.DataFrame(pLDDT_ps, columns=['atom'])
             st.write(af3ps_df)
             #explode dataframe into multiple columns to isolate pLDDT scores
             af3ps_df[['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','resn','17','18','pos','20','21','22','23','24','25','26','27','28','pLDDT','30','31','32','33','34']] = af3ps_df['atom'].str.split(' ',expand=True) #\t for tab delimited
