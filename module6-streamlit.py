6d0602d87998d95be17a0a1c8a77adbaf0d18947
Maanas Sathaye <maanas.sathaye@gmail.com>
2025-10-01 20:34:40 -0500

    use tabs instead of spaces for vec ops

--- module6-streamlit.py
+++ module6-streamlit.py
@@ -120,13 +120,12 @@ if st.button('read in clustal alignment file'):
             #make a list w numbers for 1 through number of residues
             num_resi = list(range(1,int(af3ps_df.iloc[-1,9])))
 			
-			
 			grouped = af3ps_df.groupby(af3ps_df.iloc[:, 9].astype(int))
-				pLDDT_averages = [grouped.get_group(i).iloc[:, 15].astype(float).mean() for i in num_resi]
-				resn = [grouped.get_group(i).iloc[0, 6] for i in num_resi]
-				st.write(pLDDT_averages)
-				st.write(resn)
-        
+			pLDDT_averages = [grouped.get_group(i).iloc[:, 15].astype(float).mean() for i in num_resi]
+			resn = [grouped.get_group(i).iloc[0, 6] for i in num_resi]
+			st.write(pLDDT_averages)
+			st.write(resn)
+			
             '''
             for i in num_resi:
                 temp_list = []
